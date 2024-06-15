1ee151b0e8193b343f91630a7001a4b4b7d47118
RaKalik963 <andrejmahot646@gmail.com>
2024-06-15 16:50:54 +0300

    244234324

--- main.cpp
+++ main.cpp
@@ -3,28 +3,50 @@
 using namespace std;
 
 
-int* removeNegative(int* arr, int size) {
+int* remove_negative(int* arr, int size) {
     int count = 0;
     for (int i = 0; i < size; i++) {
         if (*(arr + i) >= 0) {
             count++;
         }
     }
-    int* newArr = new int[count];
+    int* new_arr = new int[count];
     int index = 0;
     for (int i = 0; i < size; i++) {
         if (*(arr + i) >= 0) {
-            *(newArr + index) = *(arr + i);
+            *(new_arr + index) = *(arr + i);
             index++;
         }
     }
-    return newArr;
+	cout << "Changed array ";
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << new_arr[i] << " ";
+	}
+    return new_arr;
 
 }
 
 int main()
 {
+	srand(time(0));
 
+	int size;
+	cout << "enter size of array ";
+	cin >> size;
+	int* arr = new int[size];
+
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 20 - 10;
+	}
+	cout << "Array ";
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	remove_negative(arr, size);
 	return 0;
 	system("pause");
 }
