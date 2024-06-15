ddb045ad73f55cdaedd71cfe90628360531170a8
RaKalik963 <andrejmahot646@gmail.com>
2024-06-15 16:33:20 +0300

    434

--- main.cpp
+++ main.cpp
@@ -3,7 +3,24 @@
 using namespace std;
 
 
+int* removeNegative(int* arr, int size) {
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        if (*(arr + i) >= 0) {
+            count++;
+        }
+    }
+    int* newArr = new int[count];
+    int index = 0;
+    for (int i = 0; i < size; i++) {
+        if (*(arr + i) >= 0) {
+            *(newArr + index) = *(arr + i);
+            index++;
+        }
+    }
+    return newArr;
 
+}
 
 int main()
 {
