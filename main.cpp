1eb3cd9e8454207e2f6a85938f0d1f3056841b43
RaKalik963 <andrejmahot646@gmail.com>
2024-06-25 18:49:47 +0300

    lab-4

--- main.cpp
+++ main.cpp
@@ -2,50 +2,55 @@
 #include <ctime>
 using namespace std;
 
-
-int* remove_negative(int* arr, int size) {
+int* remove_negatives(int* arr, int size, int* new_size) {
     int count = 0;
-    for (int i = 0; i < size; i++) {
-        if (*(arr + i) >= 0) {
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] >= 0) {
             count++;
         }
     }
-    int* new_arr = new int[size];
-    int index = 0;
-	for (int i = 0; i < size; i++) {
-		if (i >= count) {
-			new_arr[i] =0;
-		}
-	}
-	cout << "Changed array ";
-	for (size_t i = 0; i < size; i++)
-	{
-		cout << new_arr[i] << " ";
-	}
-    return new_arr;
 
+    *new_size = count;
+    int* new_arr = new int[count];
+    int j = 0;
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] >= 0) {
+            new_arr[j++] = arr[i];
+        }
+    }
+
+    return new_arr;
 }
 
-int main()
-{
-	srand(time(0));
-	int* new_arr = new int;
-	int size;
-	cout << "enter size of array ";
-	cin >> size;
-	int* arr = new int[size];
+int main() {
+    srand(time(0));
 
-	for (size_t i = 0; i < size; i++)
-	{
-		arr[i] = rand() % 20 - 10;
-	}
-	cout << "Array ";
-	for (size_t i = 0; i < size; i++)
-	{
-		cout << arr[i] << " ";
-	}
-	cout << endl;
-	remove_negative(arr, size);
-	return 0;
-	system("pause");
+    int size;
+    cout << "Enter size of the array: ";
+    cin >> size;
+    int* arr = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 20 - 10;
+    }
+
+    cout << "Array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    int new_size;
+    int* new_arr = remove_negatives(arr, size, &new_size);
+
+    cout << "Array without negatives: ";
+    for (int i = 0; i < new_size; i++) {
+        cout << new_arr[i] << " ";
+    }
+    cout << endl;
+
+    delete[] arr;
+    delete[] new_arr;
+
+    return 0;
 }
