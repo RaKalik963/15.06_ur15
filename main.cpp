7c0641cd57662bc779e05546cb9cd03864e3ef84
RaKalik963 <andrejmahot646@gmail.com>
2024-06-25 18:52:31 +0300

    lab-6

--- main.cpp
+++ main.cpp
@@ -2,12 +2,67 @@
 #include <ctime>
 using namespace std;
 
+void paste_elements(int* A, int M, int* B, int N, int index, int* C) {
+    for (int i = 0; i < index; i++) {
+        C[i] = A[i];
+    }
+    for (int i = 0; i < N; i++) {
+        C[index + i] = B[i];
+    }
+    for (int i = index; i < M; i++) {
+        C[N + i] = A[i];
+    }
+}
 
+int main() {
+    srand(time(0));
 
+    int M, N, index;
+    cout << "Enter size of array A: ";
+    cin >> M;
+    int* A = new int[M];
 
-int main()
-{
+    for (int i = 0; i < M; i++) {
+        A[i] = rand() % 10;
+    }
 
-	return 0;
-	system("pause");
+    cout << "Enter size of array B: ";
+    cin >> N;
+    int* B = new int[N];
+
+    for (int i = 0; i < N; i++) {
+        cout << "Enter element to add: ";
+        cin >> B[i];
+    }
+
+    cout << "Enter index to insert elements: ";
+    cin >> index;
+
+    if (index < 0 || index > M) {
+        cout << "Invalid index!" << endl;
+        delete[] A;
+        delete[] B;
+        return 1;
+    }
+
+    cout << "Array A: ";
+    for (int i = 0; i < M; i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    int* C = new int[M + N];
+    paste_elements(A, M, B, N, index, C);
+
+    cout << "New array: ";
+    for (int i = 0; i < M + N; i++) {
+        cout << C[i] << " ";
+    }
+    cout << endl;
+
+    delete[] A;
+    delete[] B;
+    delete[] C;
+
+    return 0;
 }
